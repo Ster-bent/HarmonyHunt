bf3b6ff84f238aec269764c0271c71e08258aa15
Sterbent <104021908+Ster-bent@users.noreply.github.com>
2024-12-23 23:15:58 +0000

    update format.py

--- src/songdata/jsformatpy/format.py
+++ src/songdata/jsformatpy/format.py
@@ -1,13 +1,29 @@
 #JSON File Formatter for Songs
 import os
-selectFile = "songs.txt"
+selectFile = "/workspaces/HarmonyHunt/src/songdata/jsformatpy/songs.txt"
+global song
+global songNames
 
-global songs
-
-def scan_file(selectFile):
+def formfile(selectFile):
+    global song
+    global songNames
+    num = ["0","1","2","3","4","5","6","7","8","9"]
     with open(selectFile, encoding="utf-8") as file:
-        songs = file.read()
-        print(songs)
+        #Line number for file is subject to change
+
+        #* Double Check outputs and debug
+        for i in range(74):
+            song = file.readlines(i)
+            song = song.split()
+            for j in range(len(num)):
+                for i in range(1):
+                    if song[i] == num[j]:
+                        song.remove(i)
+
+
+            songNames = []
+            songNames.append(song)
+            print(song)
     
-scan_file(selectFile)
+formfile(selectFile)
 print("done")
