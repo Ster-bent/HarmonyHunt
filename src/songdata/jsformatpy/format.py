106001a5054f34dd5dd2b81e7673857c6f461799
Sterbent <104021908+Ster-bent@users.noreply.github.com>
2024-12-23 22:57:10 +0000

    merge

---
+++ src/songdata/jsformatpy/format.py
@@ -0,0 +1,12 @@
+#JSON File Formatter for Songs
+import os
+selectFile = "songs.txt"
+
+global songs
+
+def scan_file(selectFile):
+    with open(selectFile, encoding="utf-8") as file:
+        songs = file.read()
+        print(songs)
+    
+scan_file(selectFile)
