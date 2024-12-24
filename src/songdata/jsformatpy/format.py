0fe10802f5e3e135a8539b710da5662d5f03d456
Sterbent <104021908+Ster-bent@users.noreply.github.com>
2024-12-24 03:32:53 +0000

    update format.py

--- src/songdata/jsformatpy/format.py
+++ src/songdata/jsformatpy/format.py
@@ -7,16 +7,16 @@ global songNames
 def formfile(selectFile):
     global song
     global songNames
-    num = ["0","1","2","3","4","5","6","7","8","9"]
+    num = ["0","1","2","3","4","5","6","7","8","9","."]
     with open(selectFile, encoding="utf-8") as file:
         #Line number for file is subject to change
 
         #* Double Check outputs and debug
         for i in range(74):
-            song = file.readlines(i)
+            song = str(file.readlines(i))
             song = song.split()
             for j in range(len(num)):
-                for i in range(1):
+                for i in range(len(song)):
                     if song[i] == num[j]:
                         song.remove(i)
 
